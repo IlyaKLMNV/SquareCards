dfe9c437c7d892b5993d8627084f3fd918bff84e
Klimanov Ilya <iklmnv@gmail.com>
2022-11-11 17:43:50 +0300

    fixed height of wrapper

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-document.getElementsByClassName("fab fa-youtube")[0],addEventListener("click", function() {
-	window.open("https://www.youtube.com/")
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", function() {
+	window.open("https://www.youtube.com/");
 })
-document.getElementsByClassName("fab fa-youtube")[1],addEventListener("click", function() {
-	window.open("https://www.youtube.com/")
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", function() {
+	window.open("https://www.youtube.com/");
 })
-document.getElementsByClassName("fab fa-youtube")[2],addEventListener("click", function() {
-	window.open("https://www.youtube.com/")
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", function() {
+	window.open("https://www.youtube.com/");
 })
 
 
-document.getElementsByClassName("article")[0],addEventListener("click", function() {
-	window.open("https://www.youtube.com/")
+document.getElementsByClassName("article")[0].addEventListener("click", function() {
+	window.open("https://www.youtube.com/");
 })
-document.getElementsByClassName("article")[1],addEventListener("click", function() {
-	window.open("https://www.youtube.com/")
+document.getElementsByClassName("article")[1].addEventListener("click", function() {
+	window.open("https://www.youtube.com/");
 })
-document.getElementsByClassName("article")[2],addEventListener("click", function() {
-	window.open("https://www.youtube.com/")
+document.getElementsByClassName("article")[2].addEventListener("click", function() {
+	window.open("https://www.youtube.com/");
 })
